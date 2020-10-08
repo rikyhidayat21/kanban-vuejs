23f1e2c61c0a1337d9808fa77f2fa8498b0aece2
rikyhidayat21 <rikyhidayat21@gmail.com>
2020-10-08 14:02:39 +0700

    setup vue component homepage, loginpage, registerpage

---
+++ client/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <section>
+    <!-- navbar -->
+    <!-- <h1>{{message}}</h1> -->
+    <div class="container-flex">
+      <div class="item-flex">Logo</div>
+      <div class="item-flex" style="letter-spacing: 3px;"><b>KANBAN</b></div>
+      <div class="item-flex">Logout</div>
+    </div>
+    <!-- end of navbar -->
+    <HomePage></HomePage>
+    <LoginPage></LoginPage>
+    <RegisterPage></RegisterPage>
+
+    
+  </section>
+</template>
+
+<script>
+import HomePage from './views/HomePage'
+import LoginPage from './views/LoginPage'
+import RegisterPage from './views/RegisterPage'
+export default {
+  data() {
+    return {
+      message: 'Hello Mets'
+    }
+  },
+  components: { 
+    HomePage, LoginPage, RegisterPage 
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ client/src/views/RegisterPage.vue
@@ -0,0 +1,40 @@
+<template>
+  <!-- register page -->
+    <div class="container" id="register-page">
+      <div class="row mt-4 mb-4">
+        <div class="col-md-8 bg-info p-0">
+          <img src="../images/register.jpg" style="width: 100%;" alt="">
+        </div>
+        <div class="col-md-4 bg-dark">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title text-center"><b>WELCOME TO KANBAN</b></h5>
+              <p class="card-text text-center">Register</p>
+              <form action="">
+                <div class="form-group">
+                  <label for="exampleInputEmail2">Email address</label>
+                  <input type="email" class="form-control" placeholder="Enter email">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputPassword2">Password</label>
+                  <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password">
+                </div>
+                <button type="submit" class="btn btn-primary">Register</button>
+                <button type="submit" class="btn btn-danger float-right">Back</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'RegisterPage'
+}
+</script>
+
+<style>
+
+</style>
